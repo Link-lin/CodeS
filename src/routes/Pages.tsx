6fd4c3760cf38e725598f91bdd12e04df32fe743
Link <ylin278@wisc.edu>
2020-06-03 22:26:30 -0500

    route

--- src/routes/Pages.tsx
+++ src/routes/Pages.tsx
@@ -1,5 +1,13 @@
 import * as React from "react";
-import {connect} from "react-redux";
-import {Route, Switch} from "react-router-dom";
+import { connect } from "react-redux";
+import { Route, Switch } from "react-router-dom";
 
 import Home from "../components/pages/Home";
+
+const Pages = () => {
+    return (
+        <Switch>
+            <Route path="/" component={Home} />
+        </Switch>
+    )
+}
